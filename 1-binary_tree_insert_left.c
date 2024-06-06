c57658d7176af5ee9eedd775de8d6f167dccad49
Apinda <apinda127@gmail.com>
2024-06-06 02:09:38 +0200

    Task 1, refurbished.

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -35,7 +35,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 			{
 				newNode->left = parent->left;
 				parent->left = newNode;
-				newNode->left->parent = newNode;
+				newNode->left->parent = newNode->left;
 			}
 		}
 	}
