8164bfec12566fd85d4600e21c0645b68539c3fd
APINDA <apinda127@gmail.com>
2024-06-06 16:06:58 -0700

    Inorder traversal

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - prints a binary tree inorder.
+ * @tree: root node of the binary tree.
+ * @func: function pointer
+ * Return: nothing
+ */
+void binary_tree_inorder(binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && (*func) != NULL)
+	{
+		binary_tree_inorder(tree->left, (*func));
+		(*func)(tree->n);
+		binary_tree_inorder(tree->right, (*func));
+	}
+}
+
