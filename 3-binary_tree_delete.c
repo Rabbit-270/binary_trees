b05095eeff4c8d1e1edbb30d83a7c3642a70487a
APINDA <apinda127@gmail.com>
2024-06-06 14:27:03 -0700

    Updates

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,27 +8,11 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree != NULL)
 	{
-		return;
-	}
-
-	binary_tree_t *left = tree->left;
-	binary_tree_t *right = tree->right;
-
-	if (left == NULL && right == NULL)
-	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
 		free(tree);
-		return;
 	}
-	else if (left != NULL)
-	{
-		binary_tree_delete(left);
-	}
-	else if (right != NULL)
-	{
-		binary_tree_delete(right);
-	}
-	free(tree);
 }
 
